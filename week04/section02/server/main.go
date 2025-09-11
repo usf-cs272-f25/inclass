dc442a801ab2fe424e40cacaeb402fcf378ba98f
Phil Peterson <phpeterson@usfca.edu>
2025-09-11 16:05:09 -0700

    add 9/11 s02

---
+++ week04/section02/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func startServer(idx Index) {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query().Get("q")
+		hits := idx.Search(q)
+		fmt.Fprintln(w, "search term: ", q)
+	})
+	go http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	idx := NewIndex()
+	startServer(idx)
+	crawl(idx)
+	for {
+		time.Sleep(100 * time.Millisecond)
+	}
+}
