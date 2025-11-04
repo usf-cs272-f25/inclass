c0b6e6c16f1d4612ac6e54efe62f0dbf32eecbae
Phil Peterson <phpeterson@usfca.edu>
2025-11-04 09:45:29 -0800

    add 11/4 s01

---
+++ week12/section01/tools/go.mod
@@ -0,0 +1,5 @@
+module demo
+
+go 1.25.0
+
+require github.com/sashabaranov/go-openai v1.41.2 // indirect
---
+++ week12/section01/tools/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/sashabaranov/go-openai"
+	"github.com/sashabaranov/go-openai/jsonschema"
+)
+
+func main() {
+	// Use JSONSchema to describe the parameters and their types
+	params := jsonschema.Definition{
+		Type: jsonschema.Object,
+		Properties: map[string]jsonschema.Definition{
+			"location": {
+				Type:        jsonschema.String,
+				Description: "The location where we want the weather forcast",
+			},
+		},
+		Required: []string{"location"},
+	}
+
+	// Make a function using those parameters
+	f := openai.FunctionDefinition{
+		Name:        "get_current_weather",
+		Description: "The city and state, e.g. San Francisco, CA",
+		Parameters:  params,
+	}
+
+	// Make a tool using the function
+	t := openai.Tool{
+		Type:     openai.ToolTypeFunction,
+		Function: &f,
+	}
+
+	// Use a slice of ChatCompletionMessage to maintain context with the LLM
+	dialogue := []openai.ChatCompletionMessage{
+		{
+			Role:    openai.ChatMessageRoleUser,
+			Content: "What is the weather in San Francisco?",
+		},
+	}
+
+	req := openai.ChatCompletionRequest{
+		Model:    openai.GPT5,
+		Messages: dialogue,
+		Tools:    []openai.Tool{t},
+	}
+
+	client := openai.NewClient(os.Getenv("OPENAI_PROJECT_KEY"))
+	resp, err := client.CreateChatCompletion(context.TODO(), req)
+	if err != nil {
+		log.Fatal("CreateChatCompletion: ", err)
+	}
+	msg := resp.Choices[0].Message
+
+	if len(msg.ToolCalls) != 1 {
+		log.Fatal("expected one tool call")
+	}
+
+	dialogue = append(dialogue, msg)
+
+	newmsg := openai.ChatCompletionMessage{
+		Role:       openai.ChatMessageRoleTool,
+		Content:    "65 and Sunny",
+		Name:       msg.ToolCalls[0].Function.Name,
+		ToolCallID: msg.ToolCalls[0].ID,
+	}
+
+	dialogue = append(dialogue, newmsg)
+
+	newreq := openai.ChatCompletionRequest{
+		Model:    openai.GPT5,
+		Messages: dialogue,
+		Tools:    []openai.Tool{t},
+	}
+	resp, err = client.CreateChatCompletion(context.TODO(), newreq)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(resp.Choices[0].Message.Content)
+}
